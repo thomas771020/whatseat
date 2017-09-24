b6a56fc5fedf18754b29804cef9f4894de2acf42
ThomasLee <thomas771020@gmail.com>
2017-09-24 12:26:05 +0800

    Update UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { OptionService } from './option.service';
 export class AppComponent {
 
   selected = '?';
+  running = false;
 
   constructor(private optionService: OptionService) {
   }
@@ -19,10 +20,12 @@ export class AppComponent {
     const sn = this.random(0, optionCount - 1);
     let n = optionCount * 5 + sn - 1;
     this.optionService.setCurrentIndex(0);
+    this.running = true;
     const intervalId = window.setInterval(() => {
       this.selected = this.optionService.next();
       if (n-- <= 0) {
         window.clearInterval(intervalId);
+        this.running = false;
       }
     }, 60);
   }
